272040a1193b0c2ccebe6f7981af7aecb69a76e2
ThomasSouly1409 <thomas.souly@gmail.com>
2024-04-21 22:05:32 +0200

    test 2

--- server/saveUser.js
+++ server/saveUser.js
@@ -17,6 +17,16 @@ async function main() {
     const allUsers = await getAllUsers();
     console.log('Voici tous les utilisateurs:', allUsers);
     
+<<<<<<< Updated upstream
+=======
+    //const userIdToUpdate = 1;
+    //const updatedUser = await updateUser(userIdToUpdate, "NouveauNom", "NouveauMotDePasse");
+    //console.log('Utilisateur mis à jour avec succès:', updatedUser);
+    
+    const userIdToDelete = 2;
+    await deleteUser(userIdToDelete);
+    console.log('Utilisateur supprimé avec succès');
+>>>>>>> Stashed changes
   } catch (error) {
     console.error('Une erreur s\'est produite:', error);
   }
